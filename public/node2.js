1d57ac5569b347bebae735db312681cea8f7e6a2
rheinjhanne <b2028rjppuga@pshs.edu.ph>
2025-01-30 10:24:03 +0800

    Add files via upload

---
+++ public/node2.js
@@ -0,0 +1,9 @@
+// just buttons to go back to homepage or studio
+
+document.getElementById("btnback1").onclick = function () {
+  location.href = "index.html";
+}
+
+document.getElementById("btnstudio").onclick = function () {
+  location.href = "page3.html";
+}
---
+++ public/node3.js
@@ -0,0 +1,50 @@
+// just buttons to go back to dress up
+document.getElementById("btnback1").onclick = function () {
+  location.href = "page2.html";
+}
+
+// button to change the display name of the character
+document.getElementById("changename").onclick = function changename () {
+  document.getElementById("name").innerHTML = window.prompt("Enter desired name");
+}
+
+//button to change the dialogue of the character
+document.getElementById("changedialogue").onclick = function changedialogue () {
+
+  var dialogueinput = document.getElementById("dialogueinput").value;
+  
+  document.getElementById("dialogue").innerHTML = dialogueinput;
+}
+
+// turn elements into an array so its easier to reuse
+const ids = [
+  "title",
+  "caption",
+  "hrtlndecor",
+  "changename",
+  "changebackground",
+  "dialogueinput",
+  "changedialogue"
+];
+ 
+//button to hide the other elements (for when user wants a clearer screen)
+document.getElementById("hideshow").onclick = function hide () {
+  document.getElementById("hideshow").style.display = "none";
+  
+  for (let i = 0; i < ids.length; i++) {
+  document.getElementById(ids[i]).style.display = "none";
+}
+  
+document.getElementById("showhide").style.display = "inline";
+}
+
+//buttons to show the elements back
+document.getElementById("showhide").onclick = function hide () {
+  document.getElementById("showhide").style.display = "none";
+  
+    for (let i = 0; i < ids.length; i++) {
+  document.getElementById(ids[i]).style.display = "inline";
+}
+  
+document.getElementById("hideshow").style.display = "inline";
+}
